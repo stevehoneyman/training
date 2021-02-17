225444a40a493ee82bcb28b32adcea3850d3bb80
hannah <hannahloubaker@outlook.com>
2021-02-17 17:55:52 +0000

    Set up style.css and enqueued stylesheet and main script

--- functions.php
+++ functions.php
@@ -1,9 +1,14 @@
 <?php
 // add stylesheet(s)
-
+wp_enqueue_style( 'style', get_stylesheet_uri() );
+wp_enqueue_style( 'app', get_template_directory_uri() . '/css/app.css',false,'1.1','all');
 
 // add scripts
+wp_enqueue_script( 'main_script', get_template_directory_uri() . '/js/scripts.js', array('jquery') , '' , true);
 
 
 // add featured image
+add_theme_support('post-thumbnails');
+
 // add excerpts
+add_post_type_support( 'page', 'excerpt' );
