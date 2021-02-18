941bbcbb41789d919c0a2a8d947e24ddac365864
hannah <hannahloubaker@outlook.com>
2021-02-18 10:44:36 +0000

    Set up a loop and installed gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,22 @@
-// var gulp = require('gulp');
-// var sass = require('gulp-sass');
-//
-// // check Gulp's running
-// var checkDate = new Date();
-//
-// gulp.task('default', function(done) {
-// 	console.log('Gulp\'s running in the cornwall advocacy theme on: ' + checkDate);
-//   done()
-// });
-//
-// // compile scss files into app.css
-// gulp.task('sass', function(){
-//   return gulp.src('css/scss/app.scss')
-//     .pipe(sass({outputStyle:'expanded'}))
-//     .pipe(gulp.dest('css'))
-// });
-//
-// // watch
-// gulp.task('watch', function(){
-//   gulp.watch('css/scss/**/*.scss', gulp.series(['sass']));
-// });
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+// check Gulp's running
+var checkDate = new Date();
+
+gulp.task('default', function(done) {
+	console.log('Gulp\'s running in the training theme on: ' + checkDate);
+  done()
+});
+
+// compile scss files into app.css
+gulp.task('sass', function(){
+  return gulp.src('css/scss/app.scss')
+    .pipe(sass({outputStyle:'expanded'}))
+    .pipe(gulp.dest('css'))
+});
+
+// watch
+gulp.task('watch', function(){
+  gulp.watch('css/scss/**/*.scss', gulp.series(['sass']));
+});
