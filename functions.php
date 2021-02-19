87e2deaa5d8bcd14053605383062628a0f92ef26
Steve Honeyman <hachimitsu@Steves-MacBook-Pro-2.local>
2021-02-19 14:39:04 +0000

    files tidied up and contents added

--- functions.php
+++ functions.php
@@ -1,10 +1,9 @@
 <?php
 // add stylesheet(s)
 function set_theme_styles() {
-  wp_enqueue_style( 'style', get_stylesheet_uri() );
+  // wp_enqueue_style( 'style', get_stylesheet_uri() ); this isn't required as WP loads by default
   wp_enqueue_style( 'main_stylesheet' , get_template_directory_uri() . '/css/app.css?v=' .time() );
 }
-
 add_action('wp_enqueue_scripts', 'set_theme_styles');
 
 // add scripts
@@ -14,30 +13,10 @@ function set_theme_scripts() {
 add_action('wp_enqueue_scripts' , 'set_theme_scripts');
 
 // enable menus in dashboard
- add_theme_support('menus');
-
-/*
-  function register_theme_menus() {
-  	register_nav_menus(
-  		array(
-  			'primary_nav_pages' => __('Primary Nav Pages'),
-        'primary_nav_cta' => __('Primary Nav CTA'),
-  			'primary_nav_sub' => __('Primaty Nav Sub'),
-  	    )
-  		);
-  	}
-  add_action('init', 'register_theme_menus');
-*/
+add_theme_support('menus');
 
 // enable featured images
 add_theme_support('post-thumbnails');
 
 // enable excerpts
 add_post_type_support( 'page', 'excerpt' );
-
-if ( function_exists('acf_add_options_page') ) {
-  acf_add_options_page();
-}
-
-// show toolbar when logged out
-// show_admin_bar( true );
--- repeaters.php
+++ repeaters.php
@@ -5,34 +5,18 @@
 	<section>
 		<h2>Cards</h2>
 
-    <?php
+    <!-- have just tidied the php up a little, realistically we didn't the else too -->
+    <?php if( have_rows('cards') ):
+      while( have_rows('cards') ) : the_row(); ?>
 
-    // Check rows exists.
-    if( have_rows('cards') ):
+      <article class="article-card">
+        <h3 class="title"><?php the_sub_field('article_title'); ?></h3>
+        <p class="description"><?php the_sub_field('article_excerpt'); ?></p>
+        <a href="<?php the_sub_field('article_link'); ?>" class="link">Read More</a>
+      </article>
 
-        // Loop through rows.
-        while( have_rows('cards') ) : the_row();
-    ?>
-
-    <article class="article-card">
-      <h3 class="title"><?php the_sub_field('article_title'); ?></h3>
-      <p class="description"><?php the_sub_field('article_excerpt'); ?></p>
-      <a href="<?php the_sub_field('article_link'); ?>" class="link">Read More</a>
-
-    </article>
-<?php
-        // End loop.
-        endwhile;
-
-    // No value.
-    else :
-?>
-    <div>
-      <p>There is no information to show</p>
-    </div>
-<?php
-    endif;
-?>
+      <?php endwhile;
+    endif; ?>
 	</section>
 
 <?php get_footer(); ?>
